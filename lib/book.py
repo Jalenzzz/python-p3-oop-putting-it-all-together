6112ee673551deaaf8c817efc02c96c98f7aa42a
Jalen Mnene <mnenejalen72@gmail.com>
2024-05-27 02:40:12 +0300

    Finished OOP lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title: str, page_count: int):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def page_count(self) -> int:
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value: int):
+        if not isinstance(value, int):
+            raise ValueError("page_count must be an integer")
+        self._page_count = value
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand: str, size: int):
+        self.brand = brand
+        self.size = size
+        self.condition = 'New'
+
+    @property
+    def size(self) -> int:
+        return self._size
+
+    @size.setter
+    def size(self, value: int):
+        if not isinstance(value, int):
+            raise ValueError("size must be an integer.")
+        else:
+            self._size = value
+
+    def repair(self):
+        print("The shoe has been repaired.")
+        self.condition = 'New'
